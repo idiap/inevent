bd9af2113486ee58efa0c19611f6c6a2956de1b6
singram <sandy.ingram@klewel.com>
2014-04-24 12:09:05 +0200

    changing static_files directory so it does not conflict with other apps

--- portal/settings.py
+++ portal/settings.py
@@ -35,18 +35,18 @@ SERVER_PATH = PROJECT_ROOT+'/static/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = '/var/www/static/inevent_project/'
 
 # NEW SINCE DJANGO 1.4
 # sudo ln -s /usr/local/lib/python2.7/dist-packages/django/contrib/admin/static/admin
 # in /var/www (remove Nginx directive for adminmedia)
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static/inevent_project/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-   os.path.join(os.path.dirname(__file__), '../static'),
+   os.path.join(os.path.dirname(__file__), '../static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.

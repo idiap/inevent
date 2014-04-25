ba964e6b930a12e24f5ab667010e83c50b263bf2
singram <sandy.ingram@klewel.com>
2014-04-25 14:23:10 +0200

    changed static url to point to server url files

--- portal/settings.py
+++ portal/settings.py
@@ -35,14 +35,14 @@ SERVER_PATH = PROJECT_ROOT+'/static/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = '/var/www/static/inevent_static/'
 
 # NEW SINCE DJANGO 1.4
 # sudo ln -s /usr/local/lib/python2.7/dist-packages/django/contrib/admin/static/admin
 # in /var/www (remove Nginx directive for adminmedia)
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://inevent.klewel.com/static/inevent_static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (

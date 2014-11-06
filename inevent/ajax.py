79561555bf826083e0c5583d9a0b7f89c1895aa0
singram <sandy.ingram@klewel.com>
2014-11-06 15:04:47 +0100

    removing all references to ibm backend servers

--- inevent/ajax.py
+++ inevent/ajax.py
@@ -34,7 +34,7 @@ from django.conf import settings
 @dajaxice_register
 def get_transcript(request,transcript_url, event_id=None):
 #	print transcript_url
-#	sample: http://srv-inevent.haifa.il.ibm.com/rest/retrieval/getTrackFile/transcript.srt?trackId=1733
+#	sample: ..../rest/retrieval/getTrackFile/transcript.srt?trackId=1733
 	data = {}
 	try:
 		response = requests.get(transcript_url)
